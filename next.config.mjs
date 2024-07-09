3839fd7989b86140dc422d9d6a14a2d08a545772
Samuel Urias <msamuelurias@gmail.com>
2024-07-09 04:14:50 -0600

    Change hero text

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix: process.env.ASSET_PREFIX,
-    basePath: process.env.BASE_PATH,
+  output: "export",
+  assetPrefix: process.env.ASSET_PREFIX,
+  basePath: process.env.BASE_PATH,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
