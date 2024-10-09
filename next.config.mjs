f3536a0a559efb04241a812bf6e5be8d799a427b
Samuel Urias <msamuelurias@gmail.com>
2024-10-08 18:39:48 -0600

    Add export to configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   assetPrefix: process.env.ASSET_PREFIX,
   basePath: process.env.BASE_PATH,
   images: { unoptimized: true },
+  output: 'export',
 };
 
 export default nextConfig;
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (<footer  className="footer border border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
         <div className="container p-12 flex justify-between">
             <span><Image src={"/images/su_attempt_5.svg"} alt={"logo"} height={25} width={25}/></span>
-            <p className="text-slate-600">All Rights Reserved</p>
+            <p className="text-slate-600">All Rights Reserved - v2.0</p>
         </div>
     </footer>)
 }
